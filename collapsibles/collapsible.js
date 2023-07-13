939368d97b0c8fd4e543de61c3768c20ed628280
unknown <danielzhou323@gmail.com>
2023-07-12 22:08:47 -0400

    css files

---
+++ collapsibles/collapsible.js
@@ -0,0 +1,27 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+/* ----------------THIS IS TO MAKE IT OPEN WITH HOVER----------------
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("mouseover", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+*/
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
